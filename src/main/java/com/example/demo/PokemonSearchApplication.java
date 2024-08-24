8490790bbdc99f2ff7986ecb49114848dd5689a3
chiaki.shimada <chiaki.shimada@cyzennt.co.jp>
2024-08-25 07:39:44 +0900

    firstCommit

---
+++ src/main/java/com/example/demo/PokemonSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonSearchApplication.class, args);
+	}
+
+}
